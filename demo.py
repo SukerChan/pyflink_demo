f38e1909dfeb76b44a6eb18abcf1da39ed87b686
陳宇龙 <chenyulong@cuhk.edu.cn>
2021-03-17 17:14:10 +0800

    更新代码

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+import os
+
 from pyflink.common.serialization import SimpleStringSchema
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.connectors import FlinkKafkaConsumer
@@ -8,9 +10,19 @@ KAFKA_PROPERTIES = {
     'group.id': 'yulong-test',
 }
 
+cur_path = os.path.dirname(os.path.realpath(__file__))
+
 
 def tutorial():
     env = StreamExecutionEnvironment.get_execution_environment()
+    jar_files = (
+        'flink-connector-kafka_2.12-1.12.2.jar',
+        'kafka-clients-2.4.1.jar',
+    )
+    jar_paths = tuple('file://' + os.path.abspath(os.path.join(cur_path, jar_file)) for jar_file in jar_files)
+
+    env.add_jars(*jar_paths)
+    env.add_classpaths(*jar_paths)
     env.set_parallelism(1)
 
     ds = env.add_source(FlinkKafkaConsumer(TOPIC, SimpleStringSchema(), KAFKA_PROPERTIES))
