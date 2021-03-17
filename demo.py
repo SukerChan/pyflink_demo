87cc67d46ef772b6c81582e746aef2c223629467
陳宇龙 <chenyulong@cuhk.edu.cn>
2021-03-17 16:47:30 +0800

    更新代码

--- demo.py
+++ demo.py
@@ -1,18 +1,20 @@
-from pyflink.common.serialization import SimpleStringEncoder
-from pyflink.common.typeinfo import Types
+from pyflink.common.serialization import SimpleStringSchema
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors import StreamingFileSink
+from pyflink.datastream.connectors import FlinkKafkaConsumer
+
+TOPIC = 'stream_latest_news'
+KAFKA_PROPERTIES = {
+    'bootstrap.servers': '172.18.168.132:9092,172.18.168.133:9092,172.18.168.134:9092',
+    'group.id': 'yulong-test',
+}
 
 
 def tutorial():
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_parallelism(1)
-    ds = env.from_collection(
-        collection=[(1, 'aaa'), (2, 'bbb')],
-        type_info=Types.ROW([Types.INT(), Types.STRING()]))
-    # ds.add_sink(StreamingFileSink
-    #             .for_row_format('/tmp/output', SimpleStringEncoder())
-    #             .build())
+
+    ds = env.add_source(FlinkKafkaConsumer(TOPIC, SimpleStringSchema(), KAFKA_PROPERTIES))
+
     ds.print()
     env.execute("tutorial_job")
 
