53706eaffd1098ca84f0a569e4335ae9066a1a6f
陳宇龙 <chenyulong@cuhk.edu.cn>
2021-03-17 16:20:10 +0800

    更新代码

---
+++ demo.py
@@ -0,0 +1,21 @@
+from pyflink.common.serialization import SimpleStringEncoder
+from pyflink.common.typeinfo import Types
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.connectors import StreamingFileSink
+
+
+def tutorial():
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+    ds = env.from_collection(
+        collection=[(1, 'aaa'), (2, 'bbb')],
+        type_info=Types.ROW([Types.INT(), Types.STRING()]))
+    # ds.add_sink(StreamingFileSink
+    #             .for_row_format('/tmp/output', SimpleStringEncoder())
+    #             .build())
+    ds.print()
+    env.execute("tutorial_job")
+
+
+if __name__ == '__main__':
+    tutorial()
